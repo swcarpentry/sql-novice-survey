df9687f09c99e072337c592e6b36d2ad221fe927
Greg Wilson <gvwilson@third-bit.com>
2016-07-30 16:53:06 -0400

    Correcting empty string vs null in database

--- bin/create-db.sql
+++ bin/create-db.sql
@@ -1,18 +1,20 @@
-/*
-Create database to be used for learners.
-The data for the database are available as CSV files.
+-- Create database to be used for learners.
+-- The data for the database are available as CSV files.
+-- For more information, see https://www.sqlite.org/cli.html#csv
 
-For more information: https://www.sqlite.org/cli.html#csv
-*/
-
+-- Generate tables.
 create table Person (id text, personal text, family text);
 create table Site (name text, lat real, long real);
 create table Visited (id text, site text, dated text);
 create table Survey (taken integer, person text, quant text, reading real);
+
+-- Import data.
 .mode csv
 .import data/person.csv Person
 .import data/site.csv Site
 .import data/survey.csv Survey
 .import data/visited.csv Visited
+
+-- Convert empty strings to NULLs.
 UPDATE Visited SET dated = null WHERE dated = '';
 UPDATE Survey SET person = null WHERE person = '';
